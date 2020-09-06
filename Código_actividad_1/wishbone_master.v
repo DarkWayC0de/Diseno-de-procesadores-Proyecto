c7941b3f570358d9431594599aa8a53be9f3b5cc
Diego_Cruz_Rodriguez <alu0101105802@ull.edu.es>
2020-09-06 21:54:21 +0100

    Impentacion de logica de sclavo wisbone

--- Código_actividad_1/wishbone_master.v
+++ Código_actividad_1/wishbone_master.v
@@ -1,8 +1,6 @@
 module whisbone_master( input wire rst_i,
 			input wire clk_i,
 			input wire ack_i,
-			input wire err_i,
-			input wire rty_i,
 			input wire [7:0] dat_i,
 			output wire [7:0] dat_o,
 			output wire [15:0] adr_o,
@@ -14,5 +12,19 @@ module whisbone_master( input wire rst_i,
 			input wire [15:0] cpu_dir,
 			input wire [7:0] cpu_data_i,
 			output wire [7:0] cpu_data_o);
+reg r_cyc_o,r_stb_o;
+always @(posedge rst_i) begin
+    r_cyc_o = 'b0;
+    r_stb_o = 'b0;
+end
+
+initial begin
+  r_cyc_o = 'b0;
+  r_stb_o = 'b0;
+end
+
+
+assign cyc_o = r_cyc_o;
+assign stb_o = r_stb_o;
 
 endmodule
--- Código_actividad_1/wishbone_slave.v
+++ Código_actividad_1/wishbone_slave.v
@@ -7,15 +7,32 @@ module whishbone_slave(	input wire rst_i,
 		       	input wire [7:0] dat_i,
 		       	output wire [7:0] dat_o,
 		       	output wire ack_o,
-		       	output wire err_o,
-                       	output wire rty_o,
 			output wire mem_cs,
 			output wire mem_we,
 			output wire mem_oe,
 			output wire [11:0] mem_dir,
 			output wire [7:0] mem_indata,
                         input wire [7:0] mem_outdata);
+reg r_ack_o, r_mem_cs;
 
+assign ack_o = r_ack_o;
 
+always @(negedge stb_i) begin 
+  r_ack_o = 'b0;
+end
 
+initial begin 
+  r_ack_o = 'b0;
+end
+
+always @(posedge clk_i) begin
+ r_ack_o = (cyc_i & stb_i);
+end
+
+assign mem_dir = adr_i;
+assign mem_indata = dat_i;
+assign dat_o = mem_outdata;
+assign mem_cs = (r_ack_o?'b1: 'b0);
+assign mem_we = we_i;
+assign mem_oe = ~ we_i;
 endmodule
